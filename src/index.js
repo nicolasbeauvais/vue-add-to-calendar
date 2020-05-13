0a851a7d5b4466aa9fd1079244acd02358149576
Nicolas Beauvais <nicolas@bvs.email>
2020-05-13 23:36:52 +0200

    Add Office 365 calendar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import AddToCalendar from './add-to-calendar';
 
-AddToCalendar.version = '1.0.5';
+AddToCalendar.version = '1.0.6';
 
 AddToCalendar.install = (Vue) => {
   Vue.component('add-to-calendar', AddToCalendar);
